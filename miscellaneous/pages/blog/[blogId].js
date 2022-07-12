430f85ff2fa7a01b3bad752db4ca739d86b3876a
Sumeetg2000 <sumeet.g@simformsolutions.com>
2022-07-12 18:23:24 +0530

    updated blog page

--- miscellaneous/pages/blog/[blogId].js
+++ miscellaneous/pages/blog/[blogId].js
@@ -21,9 +21,11 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps() {
-  const user=process.env.DB_USER
-  const password=process>env.DB_PASSWORD
-  console.log(`Connecting to database with username ${user} and password ${password}`)
+  const user = process.env.DB_USER;
+  const password = process.env.DB_PASSWORD;
+  console.log(
+    `Connecting to database with username ${user} and password ${password}`
+  );
   return {
     props: {
       title: "Article Title",
